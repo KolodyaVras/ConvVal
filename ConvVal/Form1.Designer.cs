0ab1bea8d33ac189a1d8a5eacd13642d8932c00e
Владимир Красников <vvkrasnikov_1@edu.hse.ru>
2022-08-04 21:20:11 +0500

    Добавьте файлы проекта.

---
+++ ConvVal/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace ConvVal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.boxValToConv = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.boxCelVal = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // boxValToConv
+            // 
+            this.boxValToConv.FormattingEnabled = true;
+            this.boxValToConv.Location = new System.Drawing.Point(12, 30);
+            this.boxValToConv.Name = "boxValToConv";
+            this.boxValToConv.Size = new System.Drawing.Size(400, 21);
+            this.boxValToConv.TabIndex = 0;
+            this.boxValToConv.SelectedIndexChanged += new System.EventHandler(this.TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Исходная валюта:";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 77);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 2;
+            this.txtInput.TextChanged += new System.EventHandler(this.TextChanged);
+            this.txtInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtInput_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Сумма:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Целевая валюта:";
+            // 
+            // boxCelVal
+            // 
+            this.boxCelVal.FormattingEnabled = true;
+            this.boxCelVal.Location = new System.Drawing.Point(12, 158);
+            this.boxCelVal.Name = "boxCelVal";
+            this.boxCelVal.Size = new System.Drawing.Size(400, 21);
+            this.boxCelVal.TabIndex = 5;
+            this.boxCelVal.TextChanged += new System.EventHandler(this.TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 316);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Итог:";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(57, 316);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(355, 20);
+            this.txtResult.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 349);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.boxCelVal);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.boxValToConv);
+            this.Name = "Form1";
+            this.Text = "Конвертер валют";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox boxValToConv;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox boxCelVal;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ ConvVal/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ConvVal.DailyInfo;
+
+namespace ConvVal
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, double> dict = new Dictionary<string, double>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            FillDict(ref dict);
+            AddItems(ref boxValToConv, dict);
+            AddItems(ref boxCelVal, dict);
+
+            boxValToConv.SelectedIndex = 11;
+            boxCelVal.SelectedIndex = 0;            
+        }
+
+        public static Dictionary<string, double> FillDict(ref Dictionary<string, double> dict)
+        {
+            DailyInfoSoapClient client = new DailyInfoSoapClient();
+            dict.Add("Российский рубль", 1);
+            foreach (DataTable table in client.GetCursOnDate(On_date: DateTime.Now).Tables)
+            {
+                foreach (DataRow row in table.Rows)
+                {
+                    dict.Add(row[0].ToString().Substring(0, 50), Convert.ToDouble(row[2]) / Convert.ToDouble(row[1]));
+                }
+            }
+
+            client.Close();
+            return dict;
+        }
+
+        public static ComboBox AddItems(ref ComboBox box, Dictionary<string, double> dict)
+        {
+            foreach (var val in dict)
+                box.Items.Add(val.Key);
+
+            return box;
+        }
+
+        private void txtInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+
+            if (!Char.IsDigit(number) && number != 8 && number != 44) // цифры, клавиша BackSpace и запятая
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtInput.Text == "")
+                {
+                    txtResult.Text = "0";
+                    return;
+                }
+
+                double toConv = 1, celVal = 1;
+                foreach (var val in dict)
+                {
+                    if (val.Key == boxValToConv.SelectedItem.ToString())
+                        toConv = val.Value;
+                    if (val.Key == boxCelVal.SelectedItem.ToString())
+                        celVal = val.Value;
+
+                    txtResult.Text = (toConv * Convert.ToDouble(txtInput.Text) / celVal).ToString();
+                }
+            }
+            catch
+            {
+                txtResult.Text = "ERROR";
+            }
+        }
+    }
+}
